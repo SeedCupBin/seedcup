8e475eb84df2c1fe63b6f8b06bf7e2abf596b045
TheArchDevil <root@archdevil.net>
2024-11-18 16:21:12 +0800

    fix: math not imported

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import math
+
 def GetAxleRotationTransformation(current, target):
     return 1 if target - current > 1e-2 else -1 if target - current < -1e-2 else (target - current) / 1e-2
 def GetRectangularDistance(dis1, dis2):
