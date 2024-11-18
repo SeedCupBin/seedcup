dccd30ded89bf43e154d857b79f4c8c36d4fcc81
TheArchDevil <root@archdevil.net>
2024-11-18 16:20:30 +0800

    sync algo

--- team_algorithm.py
+++ team_algorithm.py
@@ -3,32 +3,29 @@ import numpy
 from abc import ABC, abstractmethod
 import time;
 import math;
-import utils;
+if __package__ == None or __package__ == '':
+    import utils;
+else:
+    from . import utils;
 
 class BaseAlgorithm(ABC):
     @abstractmethod 
     def get_action(self, observation):
-        """
-        输入观测值，返回动作
-        Args:
-            observation: numpy array of shape (1, 12) 包含:
-                - 6个关节角度 (归一化到[0,1])
-                - 3个目标位置坐标
-                - 3个障碍物位置坐标
-        Returns:
-            action: numpy array of shape (6,) 范围在[-1,1]之间
-        """
         pass
 
 class MyCustomAlgorithm(BaseAlgorithm):
     def __init__(self):
-        # 自定义初始化
+        self.arm2D1 = 0.425
+        self.arm2D2 = 0.39501
         pass
     def RoundNotify(self, observation):
-        print("\033[94mtarget.X = {}".format(observation[0][6]));
-        print("obstacle.X = {}".format(observation[0][9]));
-        print("target.Rot ={}".format(math.atan(observation[0][7] / observation[0][6])));
-        print("obstacle.Rot ={}\033[0m".format(math.atan(observation[0][10] / observation[0][9])));
+        self.Debug = True
+        print("\033[94mTarget: {}\033[0m".format(self.GetTargetAxleState(observation[0][6:9], observation[0][9:12])))
+        self.Debug = False
+        # print("\033[94mtarget.X = {}".format(observation[0][6]))
+        # print("obstacle.X = {}".format(observation[0][9]))
+        # print("target.Rot ={}".format(math.atan(observation[0][7] / observation[0][6])))
+        # print("obstacle.Rot ={}\033[0m".format(math.atan(observation[0][10] / observation[0][9])))
         pass
     def GetTargetAxleState(self, targetPos, obstaclePos):
         if targetPos[0] != 0: rotH = math.atan(targetPos[1] / (targetPos[0]) * 2) / math.pi - 0.25;
@@ -53,14 +50,10 @@ class MyCustomAlgorithm(BaseAlgorithm):
     def GetAction(self, axleState, targetPos, obstaclePos):
         # Arguments are splitted here.
         action = [0, 0, 0, 0, 0, 0];
-        rotH = math.atan(targetPos[1] / (targetPos[0]) * 2) / math.pi - 0.25;
-        while rotH < 0: rotH += 1
-        action[0] = utils.GetAxleRotationTransformation(axleState[0], rotH)
-        # print("Target is at: {}".format(targetPos))
-        # print("Target horizontal rotation: {}".format(rotH))
-        action[1] = -1;
-        action[2] = 1;
-        action[4] = utils.GetAxleRotationTransformation(axleState[4], 0.20);
+        target = self.GetTargetAxleState(targetPos, obstaclePos);
+        # target = self.GetTargetAxleState([0, 0.9, 0.3], obstaclePos)
+        for i in range(6):
+            action[i] = utils.GetAxleRotationTransformation(axleState[i], target[i])
         return action;
         # return [0, 0, 0, 0, 0, 0]
         
