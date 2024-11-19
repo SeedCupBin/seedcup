6a67213c43e2c3768479e0eda395f0c4415cb417
TheArchDevil <root@archdevil.net>
2024-11-19 15:35:45 +0800

    env.py: config support

--- test.py
+++ test.py
@@ -1,20 +1,14 @@
 from env import Env
 from team_algorithm import MyCustomAlgorithm
-import sys
 
 def main(algorithm):
-    if sys.platform != 'win32':
-        import posix;
-        seed = int.from_bytes(posix.urandom(4));
-    else:
-        seed = 114514;
-    env = Env(is_senior = False, seed = seed, gui = True)
+    env = Env("env.cfg")
     done = False
     num_episodes = 100
     final_score = 0
     total_steps = 0
     total_distance = 0
-    print("Seed = {}".format(seed))
+    print("Seed = {}".format(env.seed))
 
     for i in range(num_episodes):
         score = 0
@@ -43,7 +37,7 @@ def main(algorithm):
     avg_distance = total_distance / num_episodes
     avg_steps = total_steps / num_episodes
 
-    print("\033[92mTest completed.\n\tTest seed:\t{}\n\tAvg. steps:\t{}\n\tAvg. distance:\t{}\n\tFinal score:\t{}\033[0m".format(seed, avg_steps, avg_distance, final_score))
+    print("\033[92mTest completed.\n\tTest seed:\t{}\n\tAvg. steps:\t{}\n\tAvg. distance:\t{}\n\tFinal score:\t{}\033[0m".format(env.seed, avg_steps, avg_distance, final_score))
 
     env.close()
     # After exiting the loop, get the total steps and final distance
