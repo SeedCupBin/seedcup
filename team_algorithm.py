d6c16a3f46c3acceae704490ebc0b1b87ca30995
TheArchDevil <root@archdevil.net>
2024-12-05 14:51:16 +0800

    update algo

--- team_algorithm.py
+++ team_algorithm.py
@@ -7,6 +7,31 @@ import math
 class Params:
     Arm2D1 = 0.425
     Arm2D2 = 0.39501
+
+class Statistics:
+    _use = False
+    _stats = {}
+
+    @staticmethod
+    def Enable():
+        Statistics._use = True
+    
+    @staticmethod
+    def IsEnabled():
+        return Statistics._use
+
+    @staticmethod
+    def Add( name : str, value = 1):
+        if Statistics._use:
+            if name in Statistics._stats:
+                Statistics._stats[name] += value
+            else:
+                Statistics._stats[name] = value
+
+    @staticmethod
+    def GetRawData():
+        return Statistics._stats
+
 class Pos2:
     # @property
     @staticmethod
@@ -58,6 +83,7 @@ class Pos3:
         return Pos3(self.ToNpArray() - other.ToNpArray())
 
 class Utils:
+    @staticmethod
     def GetAngleFromPosition(pos : Pos2):
         if pos.X == 0:
             return math.pi / 2
@@ -66,55 +92,95 @@ class Utils:
         else:
             return math.atan(pos.Y / pos.X)
 
+    @staticmethod
     def GetAxleRotationTransformation(current, target):
         return 1 if target - current > 1e-2 else -1 if target - current < -1e-2 else (target - current) / 1e-2
 
+    @staticmethod
     def GetRectangularDistance(dis1, dis2):
         return math.sqrt(dis1 * dis1 + dis2 * dis2)
 
+    @staticmethod
     def GetRectangularDistance(posArr):
         sq = 0
         for dis in posArr:
             sq += dis * dis
         return math.sqrt(sq)
 
-    def GetTriangleAngle(c, a, b):
+    @staticmethod
+    def GetSSSAngle(c, a, b):
         # Get the angle opposite to edge C
         return math.acos((a * a + b * b - c * c) / 2 / a / b)
 
-    def NormalizeAngle(angle):
+    @staticmethod
+    def NormalizeAngle(angle : float):
         angle /= 2 * math.pi
         angle -= math.floor(angle)
         return angle
 
-    def DenormalizeAngle(angle):
+    @staticmethod
+    def DenormalizeAngle(angle : float):
         return angle * 2 * math.pi
+    
+    @staticmethod
+    def NormalizeAngles(angles):
+        pass
 
+    @staticmethod
+    def DenormalizeAngles(angles : numpy.ndarray):
+        return angles * 2 * math.pi
+
+    @staticmethod
     def GetPoint2PlaneDistance(pos : Pos2, angle):
         dist = pos.Distance()
         ang1 = Utils.GetAngleFromPosition(pos)
         ang2 = angle - ang1
         # print("\033[95mAngle = {} {} {}\033[0m".format(angle, ang1, ang2))
-        return math.sin(ang2) * dist
+        return abs(math.sin(ang2) * dist)
+    
+    @staticmethod
+    def GetSASDistance(angle : float, a : float, b : float):
+        return math.sqrt(a * a + b * b - 2 * math.cos(angle) * a * b)
 
+    @staticmethod
     def GetStateHash(state):
         p = numpy.array([0, 0, 0, 0, 0, 0, 0, 0], dtype='uint8')
         for s in state:
             p ^= numpy.frombuffer(s.tobytes(), dtype='uint8')
         return int.from_bytes(p.tobytes(), byteorder = 'big')
 
-    def GetTargetAxleState2D(targetPos2D : Pos2):
+    @staticmethod
+    def GetTargetAxleState2D(targetPos2D : Pos2) -> list:
         dist = targetPos2D.Distance()
         if dist > Params.Arm2D1 + Params.Arm2D2: # Unreachable!
+            Statistics.Add("GTAS Unreachable")
             return [0.2, 0.5, 0]
-        rotV1 = Utils.GetAngleFromPosition(targetPos2D) + Utils.GetTriangleAngle(Params.Arm2D2, Params.Arm2D1, dist)
-        rotV2 = Utils.GetTriangleAngle(dist, Params.Arm2D1, Params.Arm2D2)
+        rotV1 = Utils.GetAngleFromPosition(targetPos2D) + Utils.GetSSSAngle(Params.Arm2D2, Params.Arm2D1, dist)
+        rotV2 = Utils.GetSSSAngle(dist, Params.Arm2D1, Params.Arm2D2)
         rotV3 = (1 * math.pi - rotV1 - rotV2) / math.pi / 2
         return [Utils.NormalizeAngle(rotV1), Utils.NormalizeAngle(rotV2), rotV3]
+
+    @staticmethod
+    def Get2DPositionFromAxleState(rotation : numpy.ndarray) -> Pos2:
+        angles = Utils.DenormalizeAngles(rotation)
+        disx = Utils.GetSASDistance(angles[1], Params.Arm2D1, Params.Arm2D2)
+        rdx = Utils.GetSSSAngle(Params.Arm2D2, Params.Arm2D1, disx)
+        ahx = angles[0] - rdx
+        return Pos2([disx * math.cos(ahx), disx * math.sin(ahx)])
+
+    @staticmethod
+    def GetPositionFromAxleState(rotation : numpy.ndarray) -> Pos3:
+        p2 = Utils.Get2DPositionFromAxleState(rotation[1:3])
+        rotH = Utils.DenormalizeAngle(rotation[0])
+        x = p2.X * math.cos(rotH)
+        y = p2.X * math.sin(rotH)
+        return Pos3([x, y, p2.Y])
     
-    def MeasureStateTransformationSteps(current, target):
+    @staticmethod
+    def MeasureStateTransformationSteps(current, target) -> float:
         return math.ceil(max(abs((target - current) * 360)))
-        pass
+
+# region: Strategies
 
 class Strategy(ABC):
     @abstractmethod
@@ -131,7 +197,12 @@ class StrategyDirect(Strategy):
 
     def GetArm2ObstacleDistance(self, targetPos : Pos3, obstaclePos : Pos3):
         rotH = (Utils.GetAngleFromPosition(targetPos))
-        return Utils.GetPoint2PlaneDistance(obstaclePos.XY, rotH)
+        dist = Utils.GetPoint2PlaneDistance(obstaclePos.XY, rotH)
+        targetDistH = targetPos.XY.Distance()
+        pdis = Utils.GetRectangularDistance([targetDistH - 0.245, targetPos.Z - 0.05])
+        if (pdis > Params.Arm2D1 + Params.Arm2D2):
+            return -1
+        return dist
 
     def GetTargetAxleRotation(self, targetPos : Pos3, obstaclePos : Pos3):
         rotH = (Utils.GetAngleFromPosition(targetPos))
@@ -148,7 +219,12 @@ class StrategyAlternate(Strategy):
     def GetArm2ObstacleDistance(self, targetPos : Pos3, obstaclePos : Pos3):
         targetDistH = targetPos.XY.Distance()
         rotH = (Utils.GetAngleFromPosition(targetPos) + math.asin(self.clawA / targetDistH))
-        return Utils.GetPoint2PlaneDistance(obstaclePos.XY, rotH)
+        dist = Utils.GetPoint2PlaneDistance(obstaclePos.XY, rotH)
+        distEq = math.sqrt(targetDistH * targetDistH - self.clawA * self.clawA)
+        pdis = Utils.GetRectangularDistance([distEq - 0.16, targetPos.Z - 0.05])
+        if (pdis > Params.Arm2D1 + Params.Arm2D2):
+            return -1
+        return dist
 
     def GetTargetAxleRotation(self, targetPos : Pos3, obstaclePos : Pos3):
         targetDistH = targetPos.XY.Distance()
@@ -185,6 +261,8 @@ class StrategySide(Strategy):
         targetAxleState2D = Utils.GetTargetAxleState2D(Pos2([distEq, targetPos.Z - 0.05]))
         return [rotH, targetAxleState2D[0], targetAxleState2D[1], targetAxleState2D[2], 0.5, 0.5]
 
+# endregion
+
 class BaseAlgorithm(ABC):
     @abstractmethod 
     def get_action(self, observation):
@@ -197,6 +275,7 @@ class MyCustomAlgorithm(BaseAlgorithm):
         self.SelectedStrategy = 0
         self.DynamicStrategySequence = []
         self.Strategies = []
+        self.StaticStepLimit = 0
         self.TargetRot = [0, 0, 0, 0, 0, 0]
         self.DistDir = 0
         self.DistAlt = 0
@@ -214,7 +293,7 @@ class MyCustomAlgorithm(BaseAlgorithm):
         obsPxy = obstaclePos.XY
         rotH = Utils.GetAngleFromPosition(obsPxy)
         dist = obsPxy.Distance()
-        rotDx = math.asin(0.1 / dist)
+        rotDx = math.asin(0.125 / dist)
         rotL = rotH + rotDx - math.pi * 0.5
         rotR = rotH - rotDx - math.pi * 0.5
         posL = math.tan(rotL) * planeY
@@ -223,25 +302,30 @@ class MyCustomAlgorithm(BaseAlgorithm):
         for i in range(steps):
             area = 0
             if pos.X < posL:
+                if self.Strategies[1].GetArm2ObstacleDistance(pos, obstaclePos) < 0: continue
                 targetRot = self.Strategies[1].GetTargetAxleRotation(pos, obstaclePos)
                 area = 0
                 pass # use stratrgy alternate or side
             elif pos.X > posR:
+                if self.Strategies[0].GetArm2ObstacleDistance(pos, obstaclePos) < 0: continue
                 targetRot = self.Strategies[0].GetTargetAxleRotation(pos, obstaclePos)
                 area = 1
                 pass # use strategy direct
             elif pos.Z > posT:
+                if self.Strategies[0].GetArm2ObstacleDistance(pos, obstaclePos) < 0: continue
                 targetRot = self.Strategies[0].GetTargetAxleRotation(pos, obstaclePos)
                 area = 2
                 pass # use strategy direct
             else:
                 continue
-            stepsEst = Utils.MeasureStateTransformationSteps(axleState, targetRot) + 20
+            stepsEst = Utils.MeasureStateTransformationSteps(axleState, targetRot) + 12
             if i > stepsEst:
                 self.SelectedStrategy = 0
-                print("STATICMODE AREA = {}".format(area))
+                self.StaticStepLimit = stepsEst
+                self.DynamicStrategySequence = [ 1 if area == 0 else 0, 0, 1, 2]
+                Statistics.Add("SSA{} SEL".format(area))
                 self.TargetRot = targetRot
-                self.AddStatistcis("S0 Usage", 1)
+                Statistics.Add("S0 Usage", 1)
                 return
             pos += self.TargetMotion
             if pos.X > 0.5 or pos.X < -0.5:
@@ -252,16 +336,25 @@ class MyCustomAlgorithm(BaseAlgorithm):
         self.DynamicStrategySequence = [2, 1, 0]
             
 
-    def GetTargetAxleState(self, targetPos : Pos3, obstaclePos : Pos3):
-        if self.SelectedStrategy == 0:
+    def GetTargetAxleState(self, axleState, targetPos : Pos3, obstaclePos : Pos3):
+        if self.SelectedStrategy == 0 and self.StaticStepLimit > self.Steps: # Static strategy
             return self.TargetRot
-        else:
+        else: # Dynamic strategy
+            # curPos = Utils.GetPositionFromAxleState(axleState)
+            if self.Steps < 60:
+                scale = 0.6 / targetPos.Y 
+                targetPos.X *= scale
+                targetPos.Y *= scale
+                # if self.Steps > 120:
+                    # pass
+                targetPos.Z = max(targetPos.Z, obstaclePos.Z + 0.3)
             for i in self.DynamicStrategySequence:
                 strategy = self.Strategies[i]
                 if strategy.GetArm2ObstacleDistance(targetPos, obstaclePos) > 0:
+                    Statistics.Add("DS{} SEL".format(i))
                     return strategy.GetTargetAxleRotation(targetPos, obstaclePos)
-        self.AddStatistcis("S.SEL FAIL", 1)
-        return None
+            Statistics.Add("DSS FAIL", 1)
+            return None
 
     def GetAction(self, axleState, targetPos : Pos3, obstaclePos : Pos3):
         curHash = Utils.GetStateHash(obstaclePos.ToNpArray())
@@ -276,14 +369,14 @@ class MyCustomAlgorithm(BaseAlgorithm):
             self.TargetMotion = targetPos - self.TargetMotion
             self.DetermineStratrgy(axleState, targetPos, obstaclePos, 200 - 1)
         self.Steps += 1
-        action = [0, 1, -1, 0, 0, 0]
-        target = self.GetTargetAxleState(targetPos, obstaclePos)
+        action = [0, 0, 0, 0, 0, 0]
+        target = self.GetTargetAxleState(axleState, targetPos, obstaclePos)
         if target == None:
             return action
         self.ArmStable = True
         for i in range(6):
             action[i] = Utils.GetAxleRotationTransformation(axleState[i], target[i])
-            if i != 0 and action[i] > 0.5:
+            if self.Steps < 150 and i != 0 and action[i] > 0.5:
                 self.ArmStable = False
         return action
         
@@ -292,31 +385,24 @@ class MyCustomAlgorithm(BaseAlgorithm):
         return numpy.array(self.GetAction(observation[0][0:6], Pos3(observation[0][6:9]), Pos3(observation[0][9:12])))
 
     # region: Round notification functions
-    def AddStatistcis(self, name : str, value):
-        if self.UseStatistics:
-            if name in self.Statistics:
-                self.Statistics[name] += value
-            else:
-                self.Statistics[name] = value
-        pass
-
     def NotifyTestBegin(self):
-        self.Statistics = {}
-        self.UseStatistics = True
+        Statistics.Enable()
         pass
 
     def NotifyRoundBegin(self, observation):
         pass
 
     def NotifyRoundEnd(self, result):
+        Statistics.Add("S{} Score".format(self.SelectedStrategy), result[4])
         # self.Statistics[self.Strategy][1] += result[-1]
         pass
 
     def NotifyTestEnd(self):
-        if self.UseStatistics:
+        if Statistics.IsEnabled():
+            stats = Statistics.GetRawData()
             print("\033[96mTest statistics:")
-            for key in self.Statistics.keys():
-                print("\t{} = {}".format(key, self.Statistics[key]))
+            for key in stats.keys():
+                print("\t{} = {}".format(key, stats[key]))
             print('\033[0m', end = '')
         # print("\033[96mTest statistics:\n\tS0 Choice =\t{}\n\tS0 Score =\t{}\n\tS1 Choice =\t{}\n\tS1 Score =\t{}".format(self.Statistics[0][0], self.Statistics[0][1] / self.Statistics[0][0], self.Statistics[1][0], self.Statistics[1][1] / self.Statistics[1][0]))
         pass
