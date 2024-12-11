41065bd2135af0ac3ae02d38d0bbb75dae6e9d88
TheArchDevil <root@archdevil.net>
2024-12-11 09:27:21 +0800

    config.py: support cli override

--- config.py
+++ config.py
@@ -1,4 +1,14 @@
+import sys
+
 class Config:
+    def Parse(self, raw, override = False):
+        eqIdx = raw.find('=')
+        if eqIdx == -1: raise Exception("Error on line : Not a key-value pair.")
+        key = raw[0:eqIdx].strip()
+        value = raw[eqIdx + 1:].strip()
+        if not override and self._config.get(key) != None: raise Exception("Error on line : Duplicate key '{}'.".format(key))
+        else:
+            self._config[key] = value
     def __init__(self, cfgFile):
         self._config = dict()
         with open(cfgFile) as f:
@@ -7,13 +17,11 @@ class Config:
                 cmtIdx = raw.find('#')
                 if cmtIdx != -1: raw = raw[0:cmtIdx].strip()
                 if raw == '': continue
-                eqIdx = line.find('=')
-                if eqIdx == -1: raise Exception("Error on line : Not a key-value pair.")
-                key = raw[0:eqIdx].strip()
-                value = raw[eqIdx + 1:].strip()
-                if self._config.get(key) != None: raise Exception("Error on line : Duplicate key '{}'.".format(key))
-                else:
-                    self._config[key] = value
+                self.Parse(raw)
+        for arg in sys.argv:
+            if arg.startswith("-c:"):
+                raw = arg[3:].strip()
+                self.Parse(raw, True)
     def Get(self, key):
         return self._config.get(key)
     def GetAs(self, key, type):
